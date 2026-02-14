9492990d0cfba52f1d2ba6f2ca157e14dde1b7de
giovana-lua <giovanabarbie8@gmail.com>
2026-02-13 23:14:43 -0300

    home finalizada

---
+++ app.js
@@ -0,0 +1,9 @@
+"use-strict"
+ 
+const container_home = document.getElementById('começo')
+
+const container_cursos = document.getElementById ('cursos')
+
+const container_aluno = document.getElementById ('aluno')
+
+const botoes_curso = document.getElementById ('btn-curso')
